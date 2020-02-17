277d8c7a7446407ae1950b65be2944f4baba8f03
Jonathan Chang <jkchang0@live.unc.edu>
2020-02-17 14:30:12 -0500

    Created a drive base for the wheels and arranged the sensors. Additional code for the stopwatch added.

--- jeffrey/main.py
+++ jeffrey/main.py
@@ -19,43 +19,42 @@ objectiveThreeCompleted = False
 # Wheels and sensors, respectively
 leftWheel = Motor(Port.A)
 rightWheel = Motor(Port.D)
-touchSensor = TouchSensor(Port.S2)
-ultraSonicSensor = UltrasonicSensor(Port.S3)
+ultraSonicSensor = UltrasonicSensor(Port.S1)
+touchSensor = TouchSensor(Port.S4)
+robot = DriveBase(leftWheel, rightWheel, 56, 224)
 
-# Stopwatch for objective three
-stopWatch = None
-
-## Note: 32 refers to the center button (center dark grey button in the instructions)
+## Note: 32 refers to the center button (center dark grey button in the instructions)!
+print("Reading input...")
 
 # Objective One: Odometry
-# While loop that finishes when objective one is completed
+# While loop that finishes when objective one is completed.
 while not (objectiveOneCompleted):
     if (32 in brick.buttons()):
-        # Code to move forward goes here...
+        # Move the robot forward.
+        robot.drive_time(50, 0, 5000)
 
+        # Finish the objective.
         print("Objective One Completed")
         objectiveOneCompleted = True
 
 # Buffer
 wait(1000)
+print("Reading input...")
 
 # Objective Two: Ranging
-# While loop that finishes when objective two is completed 
+# While loop that finishes when objective two is completed.
 while not (objectiveTwoCompleted):
     if (32 in brick.buttons()):
+        # Keep the robot moving until it is <= 50 cm away from the object.
+        while (ultraSonicSensor.distance() > 500):
+            # Move the robot forward.
+            robot.drive(150, 0)
 
-        while (ultraSonicSensor.distance() >= 500):
-            # Experiment with what angular velocity should be set - arg for the run method
-            leftWheel.run()
-            rightWheel.run()
-
-            # Change buffer time accordingly
+            # Buffer
             wait(500)
 
-        # End goal. Once the robot is 50cm away from the object, turn everything off and beep
+        # Finish the objective.
         print("Objective Two Completed")
-        leftWheel.stop()
-        rightWheel.stop()
         brick.sound.beep()
         objectiveTwoCompleted = True
 
@@ -63,36 +62,41 @@ while not (objectiveTwoCompleted):
 wait(1000)
 
 # Objective Three: Bumping
-# While loop that finishes when objective three is completed
+# While loop that finishes when objective three is completed.
 while not (objectiveThreeCompleted):
+    # Stopwatch for objective three; used for the goal of moving the robot backwards 50cm.
+    stopWatch = StopWatch()
+    stopWatchActivated = False
+
     if (32 in brick.buttons()):
-
-        # Code that reverses the robot goes here...
+        # While loop that runs as long as the robot has not hit an object.
         while not (touchSensor.pressed()):
-            # Experiment with what angular velocity should be set - arg for the run method
-            leftWheel.run()
-            rightWheel.run()
+            # Moves the robot forwards.
+            robot.drive(150, 0)
 
-            # Checking if the robot is ~50cm away from the objec to track how long it'll take to get back to that same position
-            # Change the margin of error dependng on the angular velocity 
-            if (ultraSonicSensor.distance() >= 480 and ultraSonicSensor.distance() <= 520):
-                stopWatch = Stopwatch()
+            # Checking if the robot is ~50cm away from the objec to track how long it'll take to get back to that same position.
+            if (ultraSonicSensor.distance() >= 490 and ultraSonicSensor.distance() <= 510 and not stopWatchActivated):
+                # Restart the stopwatch and start tracking new time.
+                stopWatch.reset()
 
-            # Change buffer time accordingly
-            wait(500)
+            # Buffer
+            wait(50)
 
         # End goal. Stopwatch is paused and the wheels will begin to turn counterclockwise for a certain duration of time, ideally back to 50cm away
         # Stopping the stopwatch and changing the direction of the wheels
+        print("Contact has been made")
+        robot.stop()
         stopWatch.pause()
+
+        # Reinitializing the wheels and drive base to go backwards
         leftWheel = Motor(Port.A, Direction.COUNTERCLOCKWISE)
         rightWheel = Motor(Port.D, Direction. COUNTERCLOCKWISE)
+        robot = DriveBase(leftWheel, rightWheel, 56, 224)
 
         # Running and stopping the wheels after a certain amount of time
-        leftWheel.run_time(..., stopWatch.time(), Stop.BRAKE)
-        rightWheel.run_time(..., stopWatch.time(), Stop.BRAKE)
-        leftWheel.stop()
-        rightWheel.stop()
+        robot.drive_time(150, 0, stopWatch.time())
         
+        # Finish the objective.
         print("Objective Three Completed")
         objectiveThreeCompleted = True
 
